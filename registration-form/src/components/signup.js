bdf4467a9a3fe32645df51615f8034fefd30c026
Domninic <liamdomnic@gmail.com>
2021-11-24 12:32:54 +0300

    react

---
+++ registration-form/src/components/signup.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import {Grid, Paper, TextField} from '@material-ui/core'
+
+import style from './style.css'
+
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
+
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
+const Signup=()=>{
+	
+	const paperStyle={padding:20, height:'70vh', width:"60vh" , margin:" 20px  auto"}
+
+	return(
+		
+		<Grid>
+			<Paper elevation={10} style={paperStyle}>
+
+			<Grid align="center">
+				<h2  className="create_account_heading">Create account</h2>
+				<Typography>  Already have an account?
+					<Link href="#"  >Sign In </Link>
+				</Typography>
+			</Grid>
+				<Grid align="center" className="username_tf"> 
+					<TextField id="outlined-basic" label="" variant="outlined" placeholder='Enter username' size='small' fullWidth  required/>
+				</Grid>
+				<Grid className="first_Last_name">
+					<TextField id="outlined-basic" label="" variant="outlined" placeholder='First Name' type= 'password'  size='small'  required/>
+					<TextField id="outlined-basic" label="" variant="outlined" placeholder='Last Name' type= 'password'  size='small'  required/>
+				</Grid>
+
+				<Grid align="center" className="email_tf"> 
+					<TextField id="outlined-basic" label="" variant="outlined" placeholder='Email' size='small' fullWidth  required/>
+				</Grid>
+
+
+				
+					<Button type='submit' color='primary' variant='contained' fullWidth>Sign Up  </Button>
+					<FormControlLabel
+
+
+						control={
+							<Checkbox
+								name="Checkb"
+								color="primary"
+
+							/>
+							}
+
+					
+						label="I have read and agree to the Terms of Service" 
+					/>
+				
+				
+				
+
+				
+			</Paper>
+		</Grid>	
+		)
+}
+
+export default Signup
